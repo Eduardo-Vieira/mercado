c59d81905886e827720cf803820651527d1a8a09
Eduardo Vieira <carlos.vieira@pmm.am.gov.br>
2017-04-25 15:22:02 -0400

    Melhorias na app inicial

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { Page2 } from '../page2/page2';
+
 @Component({
   selector: 'page-page1',
   templateUrl: 'page1.html'
@@ -10,6 +12,10 @@ export class Page1 {
 
   constructor(public navCtrl: NavController) {
     
+  }
+  doListadeCompras(){
+    // redirect
+    this.navCtrl.setRoot(Page2);
   }
 
 }
